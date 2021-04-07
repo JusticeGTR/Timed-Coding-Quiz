3f138a8043091758dee08c28a01f03f952047929
Justin Lindsey <justinlindseylhr@gmail.com>
2021-04-06 22:21:02 -0500

    updating code

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,18 @@
 //use local memory to store high scores, and pull them from there to populate the high score board and page
 
 //make all the questions an object of objects or array of objects to keep data cleaner
+
+
+
+var generateQuizEl = document.getElementById("generateQuiz");
+var countEl = document.getElementById("counter");
+var highScoreEl = document.getElementById("highScores");
+var quizEl = document.getElementById("quiz")
+var quizTextEl = document.getElementById("quizText")
+var userInputEl = document.getElementById("userInput")
+var quizTitleEl = document.getElementById("quizTitle")
+var userScoreEl = document.getElementById("userScore")
+
 var problems = {
     questionOne:{question:"Which of these are data types present in javascript?", answer1:"String, Number, Boolean, Object", answer2:"Lions, Tigers and Bears", answer3:"String, Cheddar, Mozzarella, Brie", answer4:"Jquery, Event, Debug, Terminal"},
     questionTwo:{question:"What is the difference between '==' and '==='?", answer1:"Kinda equals and Really equals", answer2:"Loosely and Strictly equals", answer3:"Double equals and Triple equals", answer4:"'and' and 'or'"},
@@ -27,33 +39,34 @@ var problems = {
     questionFive:{question:"What is used to put a comment in Javascript?", answer1:"<!-- -->", answer2:"/* */", answer3:"//", answer4:"||"}
 }
 
-
-var generateQuizEl = document.getElementById("generateQuiz");
-var countEl = document.getElementById("counter");
-var highScoreEl = document.getElementById("highScores");
-var quizEl = document.getElementById("quiz")
-var quizTextEl = document.getElementById("quizText")
-
-
 function quizTime() {
-
-    // theQuiz();
-    var timer = 5;
+    generateQuizEl.setAttribute("style", "visibility: hidden;")
+    console.log(generateQuizEl.attributes)
+    var timer = 75;
     var setTimer = setInterval(function() {
-
+        
         countEl.textContent = timer;
-
+        
         if(timer === 0){
             clearInterval(setTimer);
-            window.location = "./scores.html"
+            generateQuizEl.setAttribute("style", "visibility: visible;")
+            quizTitleEl.textContent = "Your Score:"
+            quizTextEl.textContent = "Please enter your initials:"
+            //var highScore = user score from local storage
+            var inputBox = document.createElement("input")
+            inputBox.setAttribute("type", "text")
+            userInputEl.appendChild(inputBox)
+
         }
-
+        
         timer--;
-
+        
     }, 1000);
-
+    
+    // theQuiz();
     // function theQuiz(){
-    //     var question = "";
+    //     var currentQuestion = problems.questionOne[0];
+    //     var currentAnswers = ;
     //     for(i = 0; i > problems.key[])
     //     quizTextEl.textContent = 
     // }
